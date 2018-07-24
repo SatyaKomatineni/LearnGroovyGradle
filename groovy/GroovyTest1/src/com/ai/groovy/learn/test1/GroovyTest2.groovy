fd043934d2de476983c07ceb5ac6242d34b38fc3
Satya Komatineni <satya.komatineni@gmail.com>
2018-07-24 09:22:09 -0400

    comments

--- groovy/GroovyTest1/src/com/ai/groovy/learn/test1/GroovyTest2.groovy
+++ groovy/GroovyTest1/src/com/ai/groovy/learn/test1/GroovyTest2.groovy
@@ -1,5 +1,21 @@
 package com.ai.groovy.learn.test1
 
+/*
+ * To test 
+ * 
+ * 1. dynamic properties
+ * 2. behavior of get/set
+ * 3. How a property access is resolved to a get/set method
+ * 
+ * Demonstrates
+ * 
+ * 1. A groovy class
+ * 2. Meaning of def (like an alias for Object type)
+ * 3. Using maps
+ * 4. property access
+ * 5. defining typed and untyped (def) functions
+ * 
+ */
 class GroovyTest2 {
 
 	private def propMap = [:]
@@ -15,6 +31,9 @@ class GroovyTest2 {
 		println "set method called ${name}:${value}"
 	}
 	
+	//Test what is the return value of a function
+	//when the last call does not return anything!
+	//It seem to return null in such cases
 	def f1()
 	{
 		println "f1 called"
